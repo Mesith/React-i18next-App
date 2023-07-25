bc552fa55362bd59a53ac8f4d62d4b423e10e317
mesith <mesith621@gmail.com>
2023-07-25 11:50:33 +0530

    Initial commit with react-i18n setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./i18n"; //import this
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Feild">
+          <label>{t("fname")}</label>
+          <input type="text" name="fname" />
+        </div>
+        <div className="Feild">
+          <label>{t("lname")}</label>
+          <input type="text" name="lname" />
+        </div>
+        <div className="Feild">
+        <label>{t("mobile")}</label>
+          <input type="text" name="mobile" />
+        </div>
+        <div className="Feild">
+        <label>{t("address")}</label>
+          <input type="text" name="address" />
+        </div>
+        <div className="Feild">
+        <label>{t("message")}</label>
+          <input type="text" name="messge" />
+        </div>
+        <input type="submit" name="Save" />
       </header>
     </div>
   );
